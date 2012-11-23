2b5f9c696066ae47fef68d456e08f98f18d5f73e
Adam Hellberg <adam.hellberg95@gmail.com>
2012-11-23 13:29:34 +0100

    Added a "Guess the number" game

---
+++ lua/guess_the_number.lua
@@ -0,0 +1,25 @@
+math.randomseed(os.time())
+
+local number = math.random(1, 10)
+local guess
+local max_attempts = 5
+local attempts = 0
+repeat
+	io.write(("Guess the number (%d): "):format(max_attempts - attempts))
+	guess = io.read("*n")
+	if not guess then
+		print("Your guess was not a valid number")
+	elseif guess > number then
+		print("Too high!")
+	elseif guess < number then
+		print("Too ĺow!")
+	end
+	attempts = attempts + 1
+until guess == number or attempts >= max_attempts
+
+if guess == number then
+	print("You guessed correctly in " .. attempts .. " guesses!")
+else
+	print("You have used up all your attempts and did not find the right number")
+	print("The generated number was " .. number)
+end
